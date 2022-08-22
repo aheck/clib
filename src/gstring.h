c2338519f520fb82eac9a6ba646f3c1c0295ba07
Andreas Heck <aheck@gmx.de>
2022-08-22 14:11:26 +0200

    Initial commit

---
+++ src/gstring.h
@@ -0,0 +1,425 @@
+/*
+ * GString
+ *
+ * Copyright (c) 2022 Andreas Heck <aheck@gmx.de>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files
+ * (the "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#ifndef _GSTRING_H
+#define _GSTRING_H
+
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <strings.h>
+#include <ctype.h>
+
+#define GSTRING_MIN_BUF_SIZE 32
+#define GSTRING_DEBUG 1
+
+#define TRUE  true
+#define FALSE false
+
+#define BUFSIZE (sizeof(long) * 8 + 1)
+
+char *ltoa(long N, char *str, int base)
+{
+      register int i = 2;
+      long uarg;
+      char *tail, *head = str, buf[BUFSIZE];
+
+      if (36 < base || 2 > base)
+            base = 10;                    /* can only use 0-9, A-Z        */
+      tail = &buf[BUFSIZE - 1];           /* last character position      */
+      *tail-- = '\0';
+
+      if (10 == base && N < 0L)
+      {
+            *head++ = '-';
+            uarg    = -N;
+      }
+      else  uarg = N;
+
+      if (uarg)
+      {
+            for (i = 1; uarg; ++i)
+            {
+                  register ldiv_t r;
+
+                  r       = ldiv(uarg, base);
+                  *tail-- = (char)(r.rem + ((9L < r.rem) ?
+                                  ('A' - 10L) : '0'));
+                  uarg    = r.quot;
+            }
+      }
+      else  *tail-- = '0';
+
+      memcpy(head, ++tail, i);
+      return str;
+}
+
+typedef struct GString {
+    char *str;
+    size_t len;
+    size_t allocated_len;
+} GString;
+
+GString* g_string_new(const char *init)
+{
+    size_t init_len = 0;
+    size_t buf_size = GSTRING_MIN_BUF_SIZE;
+    GString *string;
+
+    if (init != NULL) {
+        init_len = strlen(init);
+    }
+
+    if (init_len + 1 > buf_size) {
+        buf_size = init_len + 1;
+    }
+
+    string = malloc(sizeof(GString));
+    if (string == NULL) {
+        return string;
+    }
+
+    string->str = malloc(buf_size);
+    if (string->str == NULL) {
+        free(string);
+        return string;
+    }
+
+#ifdef GSTRING_DEBUG
+        memset(string->str, '+', buf_size);
+        string->str[buf_size - 1] = '\0';
+#endif
+
+    // fill GString attributes
+    if (init_len > 0) {
+        memcpy(string->str, init, init_len + 1);
+    } else {
+        string->str[0] = '\0';
+    }
+
+    string->len = init_len;
+    string->allocated_len = buf_size;
+
+    return string;
+}
+
+bool _g_string_resize(GString *string, size_t requested_size)
+{
+    char *new_buf;
+    size_t buf_size;
+
+    if (requested_size <= string->allocated_len) {
+        return false;
+    }
+
+    buf_size = requested_size * 2;
+
+    new_buf = realloc(string->str, buf_size);
+
+    if (new_buf == NULL) {
+        return false;
+    }
+
+    string->str = new_buf;
+    string->allocated_len = buf_size;
+
+#ifdef GSTRING_DEBUG
+    // FIXME
+    memset(&string->str[string->len + 1], '+', buf_size - string->len - 1);
+    string->str[buf_size - 1] = '\0';
+#endif
+
+    return true;
+}
+
+GString* g_string_assign(GString *string, const char *rval)
+{
+    size_t new_len;
+
+    if (rval == NULL) {
+        return string;
+    }
+
+    new_len = strlen(rval);
+
+    if (new_len + 1 > string->allocated_len) {
+        if (!_g_string_resize(string, new_len + 1)) {
+            return string;
+        }
+    }
+
+    memcpy(string->str, rval, new_len + 1);
+    string->len = new_len;
+
+    return string;
+}
+
+GString* g_string_append(GString *string, const char *val)
+{
+    size_t val_len;
+    size_t new_len;
+
+    if (val == NULL) {
+        return string;
+    }
+
+    val_len = strlen(val);
+    new_len = val_len + string->len;
+
+    if (new_len + 1 > string->allocated_len) {
+        if (!_g_string_resize(string, new_len + 1)) {
+            return string;
+        }
+    }
+
+    memcpy(&string->str[string->len], val, val_len + 1);
+    string->len = new_len;
+
+    return string;
+}
+
+GString* g_string_append_c(GString *string, const char c)
+{
+    size_t new_len;
+
+    new_len = string->len + 1;
+
+    if (new_len + 1 > string->allocated_len) {
+        if (!_g_string_resize(string, new_len + 1)) {
+            return string;
+        }
+    }
+
+    string->str[string->len] = c;
+    string->len = new_len;
+    string->str[string->len] = '\0';
+
+    return string;
+}
+
+GString* g_string_prepend(GString *string, const char *val)
+{
+    size_t val_len;
+    size_t new_len;
+
+    if (val == NULL) {
+        return string;
+    }
+
+    val_len = strlen(val);
+    new_len = val_len + string->len;
+
+    if (new_len + 1 > string->allocated_len) {
+        if (!_g_string_resize(string, new_len + 1)) {
+            return string;
+        }
+    }
+
+    // move current string to end
+    memmove(&string->str[val_len], string->str, string->len + 1);
+
+    // copy string that shall be prepended
+    memcpy(string->str, val, val_len);
+
+    string->len = new_len;
+
+    return string;
+}
+
+GString* g_string_prepend_c(GString *string, const char c)
+{
+    size_t new_len;
+
+    new_len = string->len + 1;
+
+    if (new_len + 1 > string->allocated_len) {
+        if (!_g_string_resize(string, new_len + 1)) {
+            return string;
+        }
+    }
+
+    // move current string to end
+    memmove(&string->str[1], string->str, string->len + 1);
+
+    // copy char that shall be prepended
+    string->str[0] = c;
+
+    string->len = new_len;
+
+    return string;
+}
+
+GString* g_string_insert(GString *string, ssize_t pos, const char *val)
+{
+    size_t val_len;
+
+    if (pos < 0) {
+        return string;
+    }
+
+    // pos beyond last char of string
+    if (pos > string->len) {
+        return string;
+    }
+
+    val_len = strlen(val);
+
+    if (val_len == 0) {
+        return string;
+    }
+
+    // enlarge buffer?
+    if (string->len + val_len + 1 > string->allocated_len) {
+        _g_string_resize(string, string->len + val_len + 1);
+    }
+
+    memmove(&string->str[pos + val_len], &string->str[pos], string->len - pos + 1);
+    memcpy(&string->str[pos], val, val_len);
+    string->len += val_len;
+
+    return string;
+}
+
+GString* g_string_erase(GString *string, ssize_t pos, ssize_t len)
+{
+    if (pos < 0) {
+        return string;
+    }
+
+    if (len == 0) {
+        return string;
+    }
+
+    // pos beyond last char of string
+    if (pos > string->len - 1) {
+        return string;
+    }
+
+    // do we erase to the end of the string?
+    if (len < 0 || (pos + len) >= string->len) {
+        string->str[pos] = '\0';
+        string->len = pos;
+        return string;
+    }
+
+    memmove(&string->str[pos], &string->str[pos + len], string->len - pos - len + 1);
+    string->len = string->len - len;
+
+    return string;
+}
+
+GString* g_string_truncate(GString *string, size_t len)
+{
+    if (len >= string->len) {
+        return string;
+    }
+
+    string->str[len] = '\0';
+    string->len = len;
+
+    return string;
+}
+
+GString* g_string_printf(GString *string, const char *format, ...)
+{
+    va_list args;
+    const char *c;
+    char last_c = 0;
+    int int_arg;
+    double double_arg;
+    char buf[20];
+    char *cur;
+
+    if (format == NULL) {
+        return string;
+    }
+
+    g_string_truncate(string, 0);
+
+    va_start(args, format);
+
+    for (c = format; *c; c++) {
+        if (last_c == '%') {
+            if (*c == '%') {
+                g_string_append_c(string, '%');
+            } else if (*c == 's') {
+                g_string_append(string, va_arg(args, char*));
+            } else if (*c == 'd') {
+                int_arg = va_arg(args, int);
+                ltoa(int_arg, (char*) &buf, 10);
+                g_string_append(string, buf);
+            } else if (*c == 'f') {
+                double_arg = va_arg(args, double);
+                snprintf(buf, sizeof(buf), "%f", double_arg);
+                g_string_append(string, buf);
+            } else if (*c == 'x' || *c == 'X') {
+                int_arg = va_arg(args, int);
+                ltoa(int_arg, (char*) &buf, 16);
+                if (*c == 'x') {
+                    for (cur = (char*) &buf; *cur; cur++) {
+                        *cur = tolower(*cur);
+                    }
+                }
+                g_string_append(string, buf);
+            }
+        } else if (*c != '%') {
+            g_string_append_c(string, *c);
+        }
+
+        last_c = *c;
+    }
+
+    va_end(args);
+
+    return string;
+}
+
+bool g_string_equal(GString *v, GString *v2)
+{
+    if (v->len != v2->len) {
+        return false;
+    }
+
+    return memcmp(v->str, v2->str, v->len) == 0;
+}
+
+char* g_string_free(GString *string, bool free_segment)
+{
+    char *segment;
+
+    if (free_segment) {
+        free(string->str);
+        free(string);
+        return NULL;
+    }
+
+    segment = string->str;
+
+    free(string);
+
+    return segment;
+}
+
+#endif
---
+++ tests/test_gstring.c
@@ -0,0 +1,427 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <check.h>
+
+#include "gstring.h"
+
+START_TEST(test_gstring_new_null)
+{
+    GString *string = NULL;
+    string = g_string_new(NULL);
+
+    ck_assert_ptr_nonnull(string);
+    ck_assert_ptr_nonnull(string->str);
+    ck_assert_int_eq(string->len, 0);
+    ck_assert_int_eq(string->allocated_len, GSTRING_MIN_BUF_SIZE);
+
+    g_string_free(string, true);
+}
+END_TEST
+
+START_TEST(test_gstring_new_empty)
+{
+    GString *string = NULL;
+    string = g_string_new("");
+
+    ck_assert_ptr_nonnull(string);
+    ck_assert_ptr_nonnull(string->str);
+
+    ck_assert_int_eq(string->len, 0);
+    ck_assert_int_eq(string->allocated_len, GSTRING_MIN_BUF_SIZE);
+
+    g_string_free(string, true);
+}
+END_TEST
+
+START_TEST(test_gstring_new_small_string)
+{
+    GString *string = NULL;
+    string = g_string_new("Hello World");
+
+    ck_assert_ptr_nonnull(string);
+    ck_assert_ptr_nonnull(string->str);
+    ck_assert_str_eq("Hello World", string->str);
+
+    ck_assert_int_eq(string->len, 11);
+    ck_assert_int_eq(string->allocated_len, GSTRING_MIN_BUF_SIZE);
+
+    g_string_free(string, true);
+}
+END_TEST
+
+START_TEST(test_gstring_new_big_string)
+{
+    GString *string = NULL;
+
+    // alphabet * 2 = 52 characters
+    string = g_string_new("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz");
+
+    ck_assert_ptr_nonnull(string);
+    ck_assert_ptr_nonnull(string->str);
+    ck_assert_str_eq("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz", string->str);
+
+    ck_assert_int_eq(string->len, 52);
+    ck_assert_int_eq(string->allocated_len, 53);
+
+    g_string_free(string, true);
+}
+END_TEST
+
+START_TEST(test_gstring_free)
+{
+    GString *string = NULL;
+    char *result = NULL;
+
+    string = g_string_new("Hello World");
+    result = g_string_free(string, true);
+    ck_assert_ptr_null(result);
+
+    string = g_string_new("Hello World");
+    result = g_string_free(string, false);
+    ck_assert_ptr_nonnull(result);
+    ck_assert_str_eq(result, "Hello World");
+}
+END_TEST
+
+START_TEST(test_gstring_assign_empty)
+{
+    GString *string = NULL;
+
+    string = g_string_new("Hello World");
+
+    g_string_assign(string, "");
+
+    ck_assert_int_eq(string->len, 0);
+    ck_assert_int_eq(string->allocated_len, GSTRING_MIN_BUF_SIZE);
+    ck_assert_str_eq("", string->str);
+
+    g_string_free(string, true);
+}
+END_TEST
+
+START_TEST(test_gstring_assign)
+{
+    GString *string = NULL;
+
+    string = g_string_new("");
+
+    g_string_assign(string, "Hello World");
+
+    ck_assert_int_eq(string->len, 11);
+    ck_assert_int_eq(string->allocated_len, GSTRING_MIN_BUF_SIZE);
+    ck_assert_str_eq(string->str, "Hello World");
+
+    g_string_free(string, true);
+}
+END_TEST
+
+START_TEST(test_gstring_append_realloc)
+{
+    GString *string = NULL;
+
+    string = g_string_new("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+
+    ck_assert_int_eq(string->len, 26);
+    ck_assert_int_eq(string->allocated_len, 32);
+    ck_assert_str_eq("ABCDEFGHIJKLMNOPQRSTUVWXYZ", string->str);
+
+    g_string_append(string, "abcdefghijklmnopqrstuvwxyz");
+
+    ck_assert_int_eq(string->len, 52);
+    // required would be 53 bytes but GString reallocs double the requested space
+    ck_assert_int_eq(string->allocated_len, 106);
+
+    ck_assert_str_eq("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz", string->str);
+
+    g_string_free(string, true);
+}
+END_TEST
+
+START_TEST(test_gstring_prepend)
+{
+    GString *string = NULL;
+
+    string = g_string_new("Hello World");
+
+    g_string_prepend(string, "*=-");
+    ck_assert_int_eq(string->len, 14);
+    ck_assert_str_eq(string->str, "*=-Hello World");
+
+    g_string_free(string, true);
+}
+END_TEST
+
+START_TEST(test_gstring_prepend_realloc)
+{
+    GString *string = NULL;
+
+    string = g_string_new("Hello World");
+    ck_assert_int_eq(string->allocated_len, GSTRING_MIN_BUF_SIZE);
+
+    g_string_prepend(string, "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz");
+    ck_assert_int_eq(string->len, 63);
+    ck_assert_str_eq(string->str, "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyzHello World");
+    ck_assert_int_eq(string->allocated_len, 64 * 2);
+
+    g_string_free(string, true);
+}
+END_TEST
+
+START_TEST(test_gstring_insert)
+{
+    GString *string = NULL;
+
+    string = g_string_new("Hello World");
+
+    g_string_insert(string, 5, " you cruel");
+    ck_assert_int_eq(string->len, 21);
+    ck_assert_str_eq(string->str, "Hello you cruel World");
+
+    g_string_insert(string, 21, "!!!");
+    ck_assert_int_eq(string->len, 24);
+    ck_assert_str_eq(string->str, "Hello you cruel World!!!");
+
+    g_string_free(string, true);
+}
+END_TEST
+
+START_TEST(test_gstring_insert_realloc)
+{
+    GString *string = NULL;
+
+    string = g_string_new("Hello World");
+
+    g_string_insert(string, 5, " ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz");
+    ck_assert_int_eq(string->len, 64);
+    ck_assert_int_eq(string->allocated_len, 130);
+    ck_assert_str_eq(string->str, "Hello ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz World");
+
+    g_string_free(string, true);
+}
+END_TEST
+
+START_TEST(test_gstring_erase)
+{
+    GString *string = NULL;
+
+    string = g_string_new("Hello you cruel World");
+    // remove " you cruel "
+    g_string_erase(string, 5, 10);
+
+    ck_assert_int_eq(string->len, 11);
+    ck_assert_str_eq("Hello World", string->str);
+
+    // remove " World"
+    g_string_erase(string, 5, 10);
+
+    ck_assert_int_eq(string->len, 5);
+    ck_assert_str_eq("Hello", string->str);
+
+    // truncate via erase
+    g_string_erase(string, 3, -1);
+    ck_assert_int_eq(string->len, 3);
+    ck_assert_str_eq("Hel", string->str);
+
+    g_string_free(string, true);
+}
+END_TEST
+
+START_TEST(test_gstring_truncate)
+{
+    GString *string = NULL;
+
+    string = g_string_new("Hello World");
+
+    g_string_truncate(string, 5);
+    ck_assert_int_eq(string->len, 5);
+    ck_assert_str_eq("Hello", string->str);
+
+    g_string_truncate(string, 0);
+    ck_assert_int_eq(string->len, 0);
+    ck_assert_str_eq("", string->str);
+
+    g_string_free(string, true);
+}
+END_TEST
+
+START_TEST(test_gstring_printf_int)
+{
+    GString *string = NULL;
+    string = g_string_new("");
+
+    g_string_printf(string, "%d", -1234);
+    ck_assert_int_eq(string->len, 5);
+    ck_assert_str_eq(string->str, "-1234");
+
+    g_string_printf(string, "%d", 1234);
+    ck_assert_int_eq(string->len, 4);
+    ck_assert_str_eq(string->str, "1234");
+
+    g_string_free(string, true);
+}
+END_TEST
+
+START_TEST(test_gstring_printf_hex)
+{
+    GString *string = NULL;
+    string = g_string_new("");
+
+    g_string_printf(string, "0x%x", 255);
+    ck_assert_int_eq(string->len, 4);
+    ck_assert_str_eq(string->str, "0xff");
+
+    g_string_printf(string, "0x%X", 255);
+    ck_assert_int_eq(string->len, 4);
+    ck_assert_str_eq(string->str, "0xFF");
+
+    g_string_free(string, true);
+}
+END_TEST
+
+START_TEST(test_gstring_printf_string)
+{
+    GString *string = NULL;
+    string = g_string_new("");
+
+    g_string_printf(string, "%s", "Hello World");
+    ck_assert_int_eq(string->len, 11);
+    ck_assert_str_eq(string->str, "Hello World");
+
+    g_string_free(string, true);
+}
+END_TEST
+
+START_TEST(test_gstring_printf_float)
+{
+    GString *string = NULL;
+    string = g_string_new("");
+
+    g_string_printf(string, "%f", 17.38);
+    ck_assert_int_eq(string->len, 9);
+    ck_assert_str_eq(string->str, "17.380000");
+
+    g_string_free(string, true);
+}
+END_TEST
+
+START_TEST(test_gstring_printf)
+{
+    GString *string = NULL;
+    string = g_string_new("");
+
+    g_string_printf(string, "String: '%s'; Number: %d; Hex: 0x%x", "STR1", 42, 0xa1);
+    ck_assert_int_eq(string->len, 37);
+    ck_assert_str_eq(string->str, "String: 'STR1'; Number: 42; Hex: 0xa1");
+
+    g_string_free(string, true);
+}
+END_TEST
+
+START_TEST(test_gstring_equal)
+{
+    GString *string1 = NULL;
+    GString *string2 = NULL;
+
+    string1 = g_string_new("Hello World");
+    string2 = g_string_new("");
+
+    ck_assert(!g_string_equal(string1, string2));
+
+    g_string_assign(string2, "Hello World");
+    ck_assert(g_string_equal(string1, string2));
+
+    g_string_assign(string2, "Hello Worlt");
+    ck_assert(!g_string_equal(string1, string2));
+
+    g_string_free(string1, true);
+    g_string_free(string2, true);
+}
+END_TEST
+
+Suite* gstring_suite(void)
+{
+    Suite *s;
+    TCase *tc_core;
+
+    s = suite_create("GString");
+
+    /* Core test case */
+    tc_core = tcase_create("Core");
+
+    tcase_add_test(tc_core, test_gstring_new_null);
+    suite_add_tcase(s, tc_core);
+
+    tcase_add_test(tc_core, test_gstring_new_empty);
+    suite_add_tcase(s, tc_core);
+
+    tcase_add_test(tc_core, test_gstring_new_small_string);
+    suite_add_tcase(s, tc_core);
+
+    tcase_add_test(tc_core, test_gstring_new_big_string);
+    suite_add_tcase(s, tc_core);
+
+    tcase_add_test(tc_core, test_gstring_free);
+    suite_add_tcase(s, tc_core);
+
+    tcase_add_test(tc_core, test_gstring_assign_empty);
+    suite_add_tcase(s, tc_core);
+
+    tcase_add_test(tc_core, test_gstring_assign);
+    suite_add_tcase(s, tc_core);
+
+    tcase_add_test(tc_core, test_gstring_append_realloc);
+    suite_add_tcase(s, tc_core);
+
+    tcase_add_test(tc_core, test_gstring_prepend);
+    suite_add_tcase(s, tc_core);
+
+    tcase_add_test(tc_core, test_gstring_prepend_realloc);
+    suite_add_tcase(s, tc_core);
+
+    tcase_add_test(tc_core, test_gstring_insert);
+    suite_add_tcase(s, tc_core);
+
+    tcase_add_test(tc_core, test_gstring_insert_realloc);
+    suite_add_tcase(s, tc_core);
+
+    tcase_add_test(tc_core, test_gstring_erase);
+    suite_add_tcase(s, tc_core);
+
+    tcase_add_test(tc_core, test_gstring_truncate);
+    suite_add_tcase(s, tc_core);
+
+    tcase_add_test(tc_core, test_gstring_printf_int);
+    suite_add_tcase(s, tc_core);
+
+    tcase_add_test(tc_core, test_gstring_printf_hex);
+    suite_add_tcase(s, tc_core);
+
+    tcase_add_test(tc_core, test_gstring_printf_string);
+    suite_add_tcase(s, tc_core);
+
+    tcase_add_test(tc_core, test_gstring_printf_float);
+    suite_add_tcase(s, tc_core);
+
+    tcase_add_test(tc_core, test_gstring_printf);
+    suite_add_tcase(s, tc_core);
+
+    tcase_add_test(tc_core, test_gstring_equal);
+    suite_add_tcase(s, tc_core);
+
+    return s;
+}
+
+int main(int argc, char **argv)
+{
+    int number_failed;
+    Suite *s;
+    SRunner *sr;
+
+    s = gstring_suite();
+    sr = srunner_create(s);
+
+    srunner_run_all(sr, CK_NORMAL);
+    number_failed = srunner_ntests_failed(sr);
+    srunner_free(sr);
+    return (number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
+}
