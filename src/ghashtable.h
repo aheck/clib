d130fd148b31ebf2f4f7577b2df8d7a3c2a810b6
Andreas Heck <aheck@gmx.de>
2022-08-24 12:14:08 +0200

    Implement hash and equal functions as well as constructors

--- src/ghashtable.h
+++ src/ghashtable.h
@@ -27,13 +27,56 @@
 #define _GHASHTABLE_H
 
 #include <stdbool.h>
+#include <stdint.h>
+#include <strings.h>
 
 #define GHASHTABLE_MIN_SLOTS 64
 #define GHASHTABLE_MAX_LOAD 0.5
 
-typedef unsigned int (*GHashFunc)(void *key);
+typedef uint32_t (*GHashFunc)(void *key);
 typedef bool (*GEqualFunc)(void *a, void *b);
 typedef void (*GDestroyNotify)(void *data);
+bool (*GHRFunc) (void *key, void *value, void *user_data);
+
+uint32_t g_int_hash(void *v)
+{
+    uint32_t x = *((uint32_t*) v);
+
+    x = ((x >> 16) ^ x) * 0x45d9f3b;
+    x = ((x >> 16) ^ x) * 0x45d9f3b;
+    x = (x >> 16) ^ x;
+
+    return x;
+}
+
+bool g_int_equal(void *v1, void *v2)
+{
+    return *((uint32_t*) v1) == *((uint32_t*) v2);
+}
+
+uint32_t g_str_hash(void *v)
+{
+    // djb2 hash function
+    uint32_t hash = 5381;
+    int c;
+    unsigned char *str = (unsigned char*) v;
+
+    while ((c = *str++)) {
+        hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+    }
+
+    return hash;
+}
+
+bool g_str_equal(void *v1, void *v2)
+{
+    return strcmp((char*) v1, (char*) v2) == 0;
+}
+
+struct GHashTableSlot {
+    void *key;
+    void *value;
+};
 
 typedef struct GHashTable {
     size_t num_slots;
@@ -42,6 +85,7 @@ typedef struct GHashTable {
     GEqualFunc key_equal_func;
     GDestroyNotify key_destroy_func;
     GDestroyNotify value_destroy_func;
+    struct GHashTableSlot *slots;
 } GHashTable;
 
 GHashTable *g_hash_table_new(GHashFunc hash_func, GEqualFunc key_equal_func)
@@ -54,29 +98,50 @@ GHashTable *g_hash_table_new(GHashFunc hash_func, GEqualFunc key_equal_func)
         return NULL;
     }
 
-    GHashTable *htable = (GHashTable*) malloc(sizeof(GHashTable));
-    if (htable == NULL) {
+    GHashTable *hash_table = (GHashTable*) malloc(sizeof(GHashTable));
+    if (hash_table == NULL) {
         return NULL;
     }
 
-    htable->num_slots = 0;
-    htable->num_used = 0;
-    htable->hash_func = hash_func;
-    htable->key_equal_func = key_equal_func;
-    htable->key_destroy_func = NULL;
-    htable->value_destroy_func = NULL;
+    hash_table->num_slots = GHASHTABLE_MIN_SLOTS;
+    hash_table->num_used = 0;
+    hash_table->hash_func = hash_func;
+    hash_table->key_equal_func = key_equal_func;
+    hash_table->key_destroy_func = NULL;
+    hash_table->value_destroy_func = NULL;
 
-    return NULL;
+    size_t buf_size = hash_table->num_slots * sizeof(struct GHashTableSlot);
+    hash_table->slots = malloc(buf_size);
+    if (hash_table->slots == NULL) {
+        free(hash_table);
+        return NULL;
+    }
+
+    memset(hash_table->slots, 0, buf_size);
+
+    return hash_table;
 }
 
 GHashTable *g_hash_table_new_full(GHashFunc hash_func, GEqualFunc key_equal_func, GDestroyNotify key_destroy_func, GDestroyNotify value_destroy_func)
 {
-    return NULL;
+    GHashTable *hash_table = g_hash_table_new(hash_func, key_equal_func);
+
+    if (hash_table == NULL) {
+        return NULL;
+    }
+
+    hash_table->key_destroy_func = key_destroy_func;
+    hash_table->value_destroy_func = value_destroy_func;
+
+    return hash_table;
 }
 
 void g_hash_table_destroy(GHashTable *hash_table)
 {
     if (hash_table) {
+        if (hash_table->slots) {
+            free(hash_table->slots);
+        }
         free(hash_table);
     }
 }
--- tests/test_ghashtable.c
+++ tests/test_ghashtable.c
@@ -4,12 +4,45 @@
 
 #include "ghashtable.h"
 
+void free_key_dummy(void *data)
+{
+}
+
+void free_value_dummy(void *data)
+{
+}
+
 START_TEST(test_ghashtable_new)
 {
     GHashTable *htable = NULL;
-    htable = g_hash_table_new(NULL, NULL);
+    htable = g_hash_table_new(g_int_hash, g_int_equal);
 
     ck_assert_ptr_nonnull(htable);
+    ck_assert_int_eq(htable->num_slots, GHASHTABLE_MIN_SLOTS);
+    ck_assert_int_eq(htable->num_used, 0);
+    ck_assert_ptr_eq(htable->hash_func, g_int_hash);
+    ck_assert_ptr_eq(htable->key_equal_func, g_int_equal);
+    ck_assert_ptr_eq(htable->key_destroy_func, NULL);
+    ck_assert_ptr_eq(htable->value_destroy_func, NULL);
+    ck_assert_ptr_nonnull(htable->slots);
+
+    g_hash_table_destroy(htable);
+}
+END_TEST
+
+START_TEST(test_ghashtable_new_full)
+{
+    GHashTable *htable = NULL;
+    htable = g_hash_table_new_full(g_int_hash, g_int_equal, free_key_dummy, free_value_dummy);
+
+    ck_assert_ptr_nonnull(htable);
+    ck_assert_int_eq(htable->num_slots, GHASHTABLE_MIN_SLOTS);
+    ck_assert_int_eq(htable->num_used, 0);
+    ck_assert_ptr_eq(htable->hash_func, g_int_hash);
+    ck_assert_ptr_eq(htable->key_equal_func, g_int_equal);
+    ck_assert_ptr_eq(htable->key_destroy_func, free_key_dummy);
+    ck_assert_ptr_eq(htable->value_destroy_func, free_value_dummy);
+    ck_assert_ptr_nonnull(htable->slots);
 
     g_hash_table_destroy(htable);
 }
@@ -26,6 +59,7 @@ Suite* ghashtable_suite(void)
     tc_core = tcase_create("Core");
 
     tcase_add_test(tc_core, test_ghashtable_new);
+    tcase_add_test(tc_core, test_ghashtable_new_full);
 
     suite_add_tcase(s, tc_core);
 
