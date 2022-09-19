69e370400acdec0988a298d418ecb6151a68422f
Andreas Heck <aheck@gmx.de>
2022-09-19 21:43:04 +0200

    Add lookup performance tests for GHashTable

--- perf_tests/perf_test_ghashtable.c
+++ perf_tests/perf_test_ghashtable.c
@@ -22,6 +22,26 @@ void measure_insert(uint32_t num_inserts)
     printf("Inserting %d elements: %fs\n", num_inserts, CALC_SECONDS(start_time, end_time));
 }
 
+void measure_lookup(uint32_t num_lookups)
+{
+    GHashTable *htable = g_hash_table_new(g_int_hash, g_int_equal);
+
+    for (uint32_t i = 0; i < num_lookups; i++) {
+        g_hash_table_insert(htable, (void*) (uint64_t) i, (void*) "Hello World");
+    }
+
+    int start_time = clock();
+
+    for (uint32_t i = 0; i < num_lookups; i++) {
+        g_hash_table_lookup(htable, (void*) (uint64_t) i);
+    }
+
+    int end_time = clock();
+    g_hash_table_destroy(htable);
+
+    printf("Looking up %d elements: %fs\n", num_lookups, CALC_SECONDS(start_time, end_time));
+}
+
 void perf_test_insert()
 {
     printf("= perf_test_insert =\n\n");
@@ -29,11 +49,28 @@ void perf_test_insert()
     measure_insert(10);
     measure_insert(100);
     measure_insert(1000);
+    measure_insert(10000);
+    measure_insert(100000);
+    measure_insert(1000000);
+}
+
+void perf_test_lookup()
+{
+    printf("= perf_test_lookup =\n\n");
+
+    measure_insert(10);
+    measure_insert(100);
+    measure_insert(1000);
+    measure_insert(10000);
+    measure_insert(100000);
+    measure_insert(1000000);
 }
 
 int main(int argc, char **argv)
 {
     perf_test_insert();
+    printf("\n\n");
+    perf_test_lookup();
 
     return 0;
 }
